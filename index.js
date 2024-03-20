ed1e999388c7ef8b275a1bf78fae7953699d90de
iManuhK <161714108+iManuhK@users.noreply.github.com>
2024-03-21 00:56:50 +0300

    adds to continuation functions lab

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // code your solution here
+function saturdayFun(item = 'roller-skate'){
+    return `This Saturday, I want to ${item}!`
+}
+saturdayFun()
+const mondayWork = function(activity = 'go to the office'){
+    return `This Monday, I will ${activity}.`
+}
+mondayWork("work from home")
+
+function wrapAdjective(flair ="*") {
+    return function (parInner = "special") {
+      return `You are ${flair}${parInner}${flair}!`;
+    };
+  }
+  wrapAdjective("%")("a dedicated programmer");
+
+
